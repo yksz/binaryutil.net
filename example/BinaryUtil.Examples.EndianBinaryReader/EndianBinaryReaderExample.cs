4ba27a5789ee43d8695ea0e60c59e0acc6731d01
yksz <yksz.dev@gmail.com>
2017-03-17 18:33:51 +0900

    example: Add examples of EndianBinaryReader/Writer

---
+++ example/BinaryUtil.Examples.EndianBinaryReader/EndianBinaryReaderExample.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryUtil.Examples
+{
+    class EndianBinaryReaderExample
+    {
+        public static void Main(string[] args)
+        {
+            ByteOrder endian = ByteOrder.BigEndian;
+
+            // write
+            var w = new ByteBuffer(32, endian);
+            var str1 = "abcd";
+            var bytes1 = Encoding.ASCII.GetBytes(str1);
+            w.Put(bytes1);
+            w.PutBoolean(true);
+            w.Put(1);
+            w.PutUInt16(2);
+            w.PutUInt32(3);
+            w.PutUInt64(4);
+            w.PutSingle(5.0f);
+            w.PutDouble(6.0);
+
+            // read
+            using (var r = new EndianBinaryReader(new MemoryStream(w.Buffer), endian))
+            {
+                var bytes2 = r.ReadBytes(bytes1.Length);
+                var str2 = Encoding.ASCII.GetString(bytes2);
+                Console.WriteLine(str2);            // => "abcd"
+                Console.WriteLine(r.ReadBoolean()); // => True
+                Console.WriteLine(r.ReadByte());    // => 1
+                Console.WriteLine(r.ReadUInt16());  // => 2
+                Console.WriteLine(r.ReadUInt32());  // => 3
+                Console.WriteLine(r.ReadUInt64());  // => 4
+                Console.WriteLine(r.ReadSingle());  // => 5
+                Console.WriteLine(r.ReadDouble());  // => 6
+            }
+        }
+    }
+}
--- example/BinaryUtil.Examples.EndianBinaryWriter/EndianBinaryWriterExample.cs
+++ example/BinaryUtil.Examples.EndianBinaryWriter/EndianBinaryWriterExample.cs
@@ -1,32 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BinaryUtil.Examples
 {
-    class ByteBufferExample
+    class EndianBinaryWriterExample
     {
         public static void Main(string[] args)
         {
             ByteOrder endian = ByteOrder.BigEndian;
 
             // write
-            var w = new ByteBuffer(32, endian);
-            var str1 = "abcd";
-            var bytes1 = Encoding.ASCII.GetBytes(str1);
-            w.Put(bytes1);
-            w.PutBoolean(true);
-            w.Put(1);
-            w.PutUInt16(2);
-            w.PutUInt32(3);
-            w.PutUInt64(4);
-            w.PutSingle(5.0f);
-            w.PutDouble(6.0);
+            byte[] buf = new byte[32];
+            byte[] bytes1;
+            using (var w = new EndianBinaryWriter(new MemoryStream(buf), endian))
+            {
+                var str1 = "abcd";
+                bytes1 = Encoding.ASCII.GetBytes(str1);
+                w.Write(bytes1);
+                w.Write(true);
+                w.Write(1);
+                w.Write(2);
+                w.Write(3);
+                w.Write(4);
+                w.Write(5.0f);
+                w.Write(6.0);
+            }
 
             // read
-            var r = new ByteBuffer(w.Buffer, endian);
+            var r = new ByteBuffer(buf, endian);
             var bytes2 = r.Get(bytes1.Length);
             var str2 = Encoding.ASCII.GetString(bytes2);
             Console.WriteLine(str2);           // => "abcd"
